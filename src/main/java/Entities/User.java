f347aef24f8e86825c468c5eee33e56ffab858bf
Joe Morris <joe@mpb.local>
2016-11-01 10:45:00 -0500

    user entity added

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -15,12 +15,29 @@ public class User {
     @Column(nullable = false)
     String password;
 
+    @Column(nullable = true)
+    String country;
+
+    @Column(nullable = true)
+    String city;
+
+    @Column(nullable = true)
+    String zip;
+
     public User() {
     }
 
     public User(String name, String password) {
         this.name = name;
         this.password = password;
+    }
+
+    public User(String name, String password, String country, String city, String zip) {
+        this.name = name;
+        this.password = password;
+        this.country = country;
+        this.city = city;
+        this.zip = zip;
     }
 
     public int getId() {
@@ -45,5 +62,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
     }
 }
