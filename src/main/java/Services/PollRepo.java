3e119d99654338cfb602b174dc91fab66c2b02a8
jkeough1987 <jkeough1987@live.com>
2016-11-01 12:23:10 -0500

    continuation of routes

--- src/main/java/Services/PollRepo.java
+++ src/main/java/Services/PollRepo.java
@@ -2,10 +2,14 @@ package Services;
 
 import Entities.Poll;
 
+import Entities.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
 
 /**
  * Created by joshuakeough on 10/31/16.
  */
 public interface PollRepo extends CrudRepository<Poll, Integer> {
+    ArrayList<Poll> findByUser(User user);
 }
